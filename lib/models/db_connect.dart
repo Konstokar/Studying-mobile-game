915f077a9b69613fe1b07d370004a040b0f66afa
Konstokar <45202593+Konstokar@users.noreply.github.com>
2024-05-04 10:37:59 +0400

    Добавлено подключение к Firebase

---
+++ lib/models/db_connect.dart
@@ -0,0 +1,17 @@
+import 'package:http/http.dart' as http;
+import 'question_model.dart';
+import 'dart:convert';
+
+class DBconnect{
+
+  final url = Uri.parse('https://simplequizapp-e2cae-default-rtdb.firebaseio.com/questions.json');
+
+  Future<void> addQuestion(Question question) async{
+    http.post(
+        url,
+        body: json.encode({
+          "title": question.title,
+          "options": question.options,
+    }));
+  }
+}
